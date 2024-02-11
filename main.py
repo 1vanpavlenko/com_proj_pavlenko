ceac5112468083f2b1d0ca96d2bf9ea0218082a4
Paschenko Maksym <maksympash@knu.ua>
2024-02-11 17:06:49 +0200

    Cleared main.py and added function decimal_to_binary

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ print(is_pow_of_five(126))
 print(Ncd(5,6))
 
 print(is_prime(13))
-print(is_prime(14))
+
+print(decimal_to_binary(24))
--- utils.py
+++ utils.py
@@ -57,3 +57,14 @@ def from_dec_to_thirteen(dec_number: int):
     else:
         return from_dec_to_thirteen(dec_number // 13) + from_dec_to_thirteen(dec_number % 13)
 
+def decimal_to_binary(decimal_number):
+    if decimal_number == 0:
+        return '0'
+    binary_number = ''
+    while decimal_number > 0:
+        remainder = decimal_number % 2
+        binary_number = str(remainder) + binary_number
+        decimal_number //= 2
+    return binary_number
+
+
